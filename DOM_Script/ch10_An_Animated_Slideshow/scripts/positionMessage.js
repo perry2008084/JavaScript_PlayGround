9c979e7c35ed9cf287a9696bdf820ae843e67486
perry <perry2008084@gmail.com>
2018-05-08 09:25:23 +0800

    增加通用动画函数

--- DOM_Script/ch10_An_Animated_Slideshow/scripts/positionMessage.js
+++ DOM_Script/ch10_An_Animated_Slideshow/scripts/positionMessage.js
@@ -5,14 +5,33 @@ function positionMessage() {
   elem.style.position = "absolute";
   elem.style.left = "50px";
   elem.style.top = "100px";
+  movement = setTimeout("moveMessage()", 5000);
 }
 
 function moveMessage() {
   if (!document.getElementById) return false;
   if (!document.getElementById("message")) return false;
   var elem = document.getElementById("message");
-  elem.style.left = "200px";
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if (xpos == 200 && ypos == 100) {
+    return true;
+  }
+  if (xpos < 200) {
+    xpos++;
+  }
+  if (xpos > 200) {
+    xpos--;
+  }
+  if (ypos < 100) {
+    ypos++;
+  }
+  if (ypos > 100) {
+    ypos--;
+  }
+  elem.style.left = xpos + 'px';
+  elem.style.top = ypos + 'px';
+  movement = setTimeout("moveMessage()", 10);
 }
 
 addLoadEvent(positionMessage);
-addLoadEvent(moveMessage);
