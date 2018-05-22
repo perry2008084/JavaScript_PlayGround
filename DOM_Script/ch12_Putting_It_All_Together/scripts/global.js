ecee5759590bb61f981afa9253400857a9e4d8e6
perry <perry2008084@gmail.com>
2018-05-22 09:08:24 +0800

    增加index.html

---
+++ DOM_Script/ch12_Putting_It_All_Together/scripts/global.js
@@ -0,0 +1,31 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement, targerElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targerElement.nextSibling);
+  }
+}
+
+function addClass(element, value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName += " ";
+    newClassName += value;
+    element.className = newClassName;
+  }
+}
