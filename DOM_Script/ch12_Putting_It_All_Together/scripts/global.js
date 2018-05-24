6041fba19cca4596f79efa2e458470b2bd9832e6
perry <perry2008084@gmail.com>
2018-05-24 20:43:13 +0800

    增加动画函数

--- DOM_Script/ch12_Putting_It_All_Together/scripts/global.js
+++ DOM_Script/ch12_Putting_It_All_Together/scripts/global.js
@@ -10,12 +10,12 @@ function addLoadEvent(func) {
   }
 }
 
-function insertAfter(newElement, targerElement) {
+function insertAfter(newElement, targetElement) {
   var parent = targetElement.parentNode;
   if (parent.lastChild == targetElement) {
     parent.appendChild(newElement);
   } else {
-    parent.insertBefore(newElement, targerElement.nextSibling);
+    parent.insertBefore(newElement, targetElement.nextSibling);
   }
 }
 
@@ -51,4 +51,88 @@ function highlightPage() {
   }
 }
 
+function moveElement(elementID, final_x, final_y, interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  if (elem.movement) {
+    clearTimeout(elem.movement);
+  }
+  if (!elem.style.left) {
+    elem.style.left = "0px";
+  }
+  if (!elem.style.top) {
+    elem.style.top = "0px";
+  }
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  var dist = 0;
+  if (xpos == final_x && ypos == final_y) {
+    return true;
+  }
+  if (xpos <  final_x) {
+    dist = Math.ceil((final_x - xpos)/10);
+    xpos += dist;
+  }
+  if (xpos > final_x) {
+    dist = Math.ceil((xpos - final_x)/10);
+    xpos = xpos - dist;
+  }
+  if (ypos < final_y) {
+    dist = Math.ceil((final_y - ypos)/10);
+    ypos = ypos + dist;
+  }
+  if (ypos > final_y) {
+    dist = Math.ceil((ypos - final_y)/10);
+    ypos = ypos - dist;
+  }
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y + "," + interval + ")";
+  elem.movement = setTimeout(repeat, interval);
+}
+
+function prepareSlideshow() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById('intro')) return false;
+  var intro  = document.getElementById('intro');
+  var slideshow = document.createElement('div');
+  var frame = document.createElement('img');
+  frame.setAttribute('src', 'images/frame.gif');
+  frame.setAttribute('alt','');
+  frame.setAttribute('id', 'frame');
+  slideshow.appendChild(frame);
+  slideshow.setAttribute('id', 'slideshow');
+  var preview = document.createElement('img');
+  preview.setAttribute('src', 'images/slideshow.gif');
+  preview.setAttribute('alt', 'a glimpse of what awaits you');
+  preview.setAttribute('id', 'preview');
+  slideshow.appendChild(preview);
+  insertAfter(slideshow, intro);
+  var links = document.getElementsByTagName('a');
+  var destination;
+  for (var i=0; i<links.length; i++) {
+    links[i].onmouseover = function() {
+      destination = this.getAttribute('href');
+      if (destination.indexOf('index.html') != -1) {
+        moveElement('preview', 0,0,5);
+      }
+      if (destination.indexOf('about.html') != -1) {
+        moveElement('preview', -150,0,5);
+      }
+      if (destination.indexOf('photos.html') != -1) {
+        moveElement('preview', -300,0,5);
+      }
+      if (destination.indexOf('live.html') != -1) {
+        moveElement('preview', -450,0,5);
+      }
+      if (destination.indexOf('contact.html') != -1) {
+        moveElement('preview', -600,0,0);
+      }
+    }
+  }
+}
+
 addLoadEvent(highlightPage);
+addLoadEvent(prepareSlideshow);
