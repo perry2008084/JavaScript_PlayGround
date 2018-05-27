014e78554822b654cd6ed4c64c1b5043816efafb
perry <perry2008084@gmail.com>
2018-05-27 11:49:16 +0800

    修改placehoder跨浏览器一致性

--- DOM_Script/ch12_Putting_It_All_Together/scripts/global.js
+++ DOM_Script/ch12_Putting_It_All_Together/scripts/global.js
@@ -313,6 +313,37 @@ function focusLabels() {
   }
 }
 
+function resetFields(whichform) {
+  //if (Modernizr.input.placeholder) return;
+  for (var i=0; i<whichform.elements.length; i++) {
+    var element = whichform.elements[i];
+    if (element.type == "submit") continue;
+    var check = element.placeholder || element.getAttribute('placeholder');
+    if (!check) continue;
+    element.onfocus = function() {
+      var text = this.placeholder || this.getAttribute('placeholder');
+      if (this.value == text) {
+        this.className = '';
+        this.value = '';
+      }
+    }
+    element.onblur = function() {
+      if (this.value == '') {
+        this.className = 'placeholder';
+        this.value = this.placeholder || this.getAttribute('placeholder');
+      }
+    }
+    element.onblur();
+  }
+}
+
+function prepareForms() {
+  for (var i=0; i<document.forms.length; i++) {
+    var thisform = document.forms[i];
+    resetFields(thisform);
+  }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
@@ -322,3 +353,4 @@ addLoadEvent(displayAbbreviations);
 addLoadEvent(stripeTables);
 addLoadEvent(hightlightRows);
 addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
